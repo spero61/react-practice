bb9e96aee7ddd8d2b0988fb92199a445436003ef
spero61 <spero61@gmail.com>
2022-06-18 00:10:48 +0900

    slate.js first impression

--- slate/src/App.js
+++ slate/src/App.js
@@ -1,34 +1,41 @@
 // Import React dependencies.
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 // Import the Slate editor factory.
-import { createEditor } from 'slate'
+import { createEditor } from 'slate';
 // Import the Slate components and React plugin.
-import { Slate, Editable, withReact } from 'slate-react'
-import logo from './logo.svg';
+import { Slate, Editable, withReact } from 'slate-react';
 import './App.css';
 
-const initialValue = []
+const initialValue = [
+  {
+    type: 'paragraph',
+    children: [{ text: 'A line of text in a paragraph.'}],
+  },
+];
+
+const customValue = [
+  {
+    type: 'paragraph',
+    children: [{ text: 'Slate.js here we go!'}],
+  },
+];
 
 function App() {
-    // Create a Slate editor object that won't change across renders.
+  // Create a Slate editor object that won't change across renders.
   const [editor] = useState(() => withReact(createEditor()));
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Slate editor={editor} value={initialValue} />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
+      <body>
+      {/* Add the editable component inside the context. */}
+      <Slate editor={editor} value={initialValue}>
+        <Editable />
+      </Slate>
+      <br></br>
+      <Slate editor={editor} value={customValue}>
+        <Editable />
+      </Slate>
+      </body>
     </div>
   );
 }
